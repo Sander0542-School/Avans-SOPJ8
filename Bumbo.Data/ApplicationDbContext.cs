021fc2a63816b772cae9b8dc2a1389d4d3cde55c
Sander Jochems <sanderjochems@hotmail.nl>
2020-10-16 12:15:34 +0200

    Add new IdentityUser to ApplicationDbContext

--- Bumbo.Data/ApplicationDbContext.cs
+++ Bumbo.Data/ApplicationDbContext.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using IdentityUser = Bumbo.Data.Models.IdentityUser;
 
 namespace Bumbo.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole<int>, int>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser>(b =>
+            {
+                b.ToTable("Users");
+            });
+
+            builder.Entity<IdentityUserClaim<int>>(b =>
+            {
+                b.ToTable("UserClaims");
+            });
+
+            builder.Entity<IdentityUserLogin<int>>(b =>
+            {
+                b.ToTable("UserLogins");
+            });
+
+            builder.Entity<IdentityUserToken<int>>(b =>
+            {
+                b.ToTable("UserTokens");
+            });
+
+            builder.Entity<IdentityRole<int>>(b =>
+            {
+                b.ToTable("Roles");
+            });
+
+            builder.Entity<IdentityRoleClaim<int>>(b =>
+            {
+                b.ToTable("RoleClaims");
+            });
+
+            builder.Entity<IdentityUserRole<int>>(b =>
+            {
+                b.ToTable("UserRoles");
+            });
         }
     }
 }
