f5f50d4333037e4a72bb5e2f576e4fece69ec97a
Sander Jochems <sanderjochems@hotmail.nl>
2020-10-16 12:20:09 +0200

    Remove Virtual Prefix from IdentityUser Model

--- Bumbo.Data/Models/IdentityUser.cs
+++ Bumbo.Data/Models/IdentityUser.cs
@@ -10,25 +10,25 @@ namespace Bumbo.Data.Models
         /// Gets or sets the first name for this user.
         /// </summary>
         [ProtectedPersonalData]
-        public virtual string FirstName { get; set; }
+        public string FirstName { get; set; }
         
         /// <summary>
         /// Gets or sets the middle name for this user.
         /// </summary>
         [ProtectedPersonalData]
-        public virtual string MiddleName { get; set; }
+        public string MiddleName { get; set; }
         
         /// <summary>
         /// Gets or sets the last name for this user.
         /// </summary>
         [ProtectedPersonalData]
-        public virtual string LastName { get; set; }
+        public string LastName { get; set; }
         
         /// <summary>
         /// Gets or sets the birthday for this user.
         /// </summary>
         [ProtectedPersonalData]
-        public virtual DateTime Birthday { get; set; }
+        public DateTime Birthday { get; set; }
         
         /// <summary>
         /// Gets or sets the birthday for this user.
@@ -37,7 +37,7 @@ namespace Bumbo.Data.Models
         [StringLength(7, MinimumLength = 6)]
         [RegularExpression(@"^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-zA-Z]{2}$")]
         [Required(ErrorMessage = "Zip Code is Required.")]
-        public virtual string ZipCode { get; set; }
+        public string ZipCode { get; set; }
         
         /// <summary>
         /// Gets or sets the house number for this user.
@@ -45,6 +45,6 @@ namespace Bumbo.Data.Models
         [ProtectedPersonalData]
         [StringLength(7, MinimumLength = 1)]
         [RegularExpression(@"^[1-9][0-9]{0,4}[a-z]{0,2}$")]
-        public virtual string HouseNumber { get; set; }
+        public string HouseNumber { get; set; }
     }
 }
