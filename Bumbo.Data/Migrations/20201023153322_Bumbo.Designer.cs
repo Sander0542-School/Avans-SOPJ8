090729bf4cf07185ce09c1e45d6b3c062e71a367
Sander Jochems <sanderjochems@hotmail.nl>
2020-10-23 17:36:49 +0200

    Add Bumbo Migration

--- Bumbo.Data/Migrations/20201023153322_Bumbo.Designer.cs
+++ Bumbo.Data/Migrations/20201023153322_Bumbo.Designer.cs
@@ -4,14 +4,16 @@ using Bumbo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bumbo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201023153322_Bumbo")]
+    partial class Bumbo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
