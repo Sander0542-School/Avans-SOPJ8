ff71fc99198852718c61a543f74060db54492421
Job <contact@chu.mk>
2020-11-02 13:07:58 +0100

    Added IEntity as base interface for all models

--- Bumbo.Data/Models/Common/BaseEntity.cs
+++ Bumbo.Data/Models/Common/BaseEntity.cs
@@ -6,7 +6,7 @@ namespace Bumbo.Data.Models.Common
     /// <summary>
     /// Base class for all non many to many based models.
     /// </summary>
-    public abstract class BaseEntity
+    public abstract class BaseEntity : IEntity
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
