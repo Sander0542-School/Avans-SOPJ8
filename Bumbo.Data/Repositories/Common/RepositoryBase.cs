4604bba998701f6d3c6bd1200b472aed98415cde
Job <contact@chu.mk>
2020-11-02 13:51:02 +0100

    Added new base repository implementation

---
+++ Bumbo.Data/Repositories/Common/RepositoryBase.cs
@@ -0,0 +1,82 @@
+﻿using Bumbo.Data.Models.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Bumbo.Data.Repositories.Common
+{
+    public abstract class RepositoryBase<TEntity> : RepositoryBase<TEntity, ApplicationDbContext> 
+        where TEntity : class, IEntity
+    {
+        public RepositoryBase(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+
+    public abstract class RepositoryBase<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : class, IEntity
+        where TContext : DbContext
+    {
+        protected readonly TContext Context;
+        protected readonly DbSet<TEntity> DbSet;
+
+        public RepositoryBase(TContext context)
+        {
+            Context = context;
+            DbSet = context.Set<TEntity>();
+        }
+
+        protected virtual IQueryable<TEntity> GetQueryBase()
+        {
+            return DbSet;
+        }
+
+        public async Task<TEntity> Add(TEntity entity)
+        {
+            await DbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<TEntity> Remove(params Expression<Func<TEntity, bool>>[] predicates)
+        {
+            var entity = await Get(predicates);
+            if (entity == null) return null;
+
+            return await Remove(entity);
+        }
+
+        public async Task<TEntity> Remove(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Deleted;
+            return entity;
+        }
+
+        public async Task<TEntity> Get(params Expression<Func<TEntity, bool>>[] predicates)
+        {
+            return await predicates
+                .Aggregate(
+                    GetQueryBase(),
+                    (query, predicate) => query.Where(predicate))
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<TEntity>> GetAll(params Expression<Func<TEntity, bool>>[] predicates)
+        {
+            return await predicates
+                .Aggregate(
+                    GetQueryBase(),
+                    (query, predicate) => query.Where(predicate))
+                .ToListAsync();
+        }
+
+        public async Task<TEntity> Update(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            
+            return entity;
+        }
+    }
+}
