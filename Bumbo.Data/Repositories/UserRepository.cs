073ff20e34e3eefbf91a08d4d70871e318c8ebf3
Sander Jochems <sanderjochems@hotmail.nl>
2020-11-13 23:38:34 +0100

    Create GetUsersAndShifts method in UserRepository

--- Bumbo.Data/Repositories/UserRepository.cs
+++ Bumbo.Data/Repositories/UserRepository.cs
@@ -1,5 +1,12 @@
-﻿using Bumbo.Data.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Bumbo.Data.Models;
+using Bumbo.Data.Models.Enums;
 using Bumbo.Data.Repositories.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bumbo.Data.Repositories
 {
@@ -7,6 +14,20 @@ namespace Bumbo.Data.Repositories
     {
         public UserRepository(ApplicationDbContext context) : base(context)
         {
+        }
+
+        public async Task<List<User>> GetUsersAndShifts(int branchId, int year, int week, params Department[] departments)
+        {
+            return await Context.Users
+                .Include(user => user.Shifts
+                    .Where(shift => departments.Contains(shift.Department))
+                    .Where(shift => shift.StartTime >= ISOWeek.ToDateTime(year, week, DayOfWeek.Monday))
+                    .Where(shift => shift.StartTime < ISOWeek.ToDateTime(year, week + 1, DayOfWeek.Monday))
+                )
+                .Include(user => user.UserAvailabilities)
+                .Include(user => user.UserAdditionalWorks)
+                // .Where(user => user.BranchId == branchId) //TODO
+                .ToListAsync();
         }
     }
 }
