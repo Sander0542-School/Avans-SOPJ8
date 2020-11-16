e51f84302c547db90111bb18ecb9a8e4cb482a6f
Sander Jochems <sanderjochems@hotmail.nl>
2020-11-16 14:36:01 +0100

    Minify UserUtil

--- Bumbo.Logic/Utils/UserUtil.cs
+++ Bumbo.Logic/Utils/UserUtil.cs
@@ -1,15 +1,13 @@
 using System;
+using System.Linq;
 using Bumbo.Data.Models;
 
 namespace Bumbo.Logic.Utils
 {
     public class UserUtil
     {
-        public static int GetAge(User user)
-        {
-            return GetAge(user, DateTime.Today);
-        }
-        
+        public static int GetAge(User user) => GetAge(user, DateTime.Today);
+
         public static int GetAge(User user, DateTime date)
         {
             var age = date.Year - user.Birthday.Year;
@@ -18,5 +16,7 @@ namespace Bumbo.Logic.Utils
 
             return age;
         }
+
+        public static string GetFullName(User user) => $"{user.FirstName} {(String.IsNullOrWhiteSpace(user.MiddleName) ? "" : user.MiddleName.Concat(" "))}{user.LastName}";
     }
 }
