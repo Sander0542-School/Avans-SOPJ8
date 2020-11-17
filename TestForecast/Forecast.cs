519a456ba15d4d93cc303ad91b32cdaf9650d003
Job <LordChunk@users.noreply.github.com>
2020-11-17 22:23:09 +0100

    Implemented WorkHoursStockClerk method

--- TestForecast/Forecast.cs
+++ TestForecast/Forecast.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Bumbo.Data.Models;
+using Bumbo.Data.Models.Enums;
 
-namespace Bumbo.Logic
+namespace TestForecast
 {
     public class Forecast
     {
+        private readonly int _expectedColi;
+        private readonly List<BranchForecastStandard> _forecastStandards;
+
+        public Forecast(int expectedColi, List<BranchForecastStandard> forecastStandards)
+        {
+            _expectedColi = expectedColi;
+            _forecastStandards = forecastStandards;
+        }
+
         public int WorkHoursCashRegister()
         {
             throw new System.NotImplementedException();
@@ -17,15 +25,18 @@ namespace Bumbo.Logic
         {
             throw new System.NotImplementedException();
         }
-
-        public int WorkHoursStockClerk()
+        /// <summary>
+        /// Calculates the total number of hours that need to be worked depending on the amount of coli that's expected
+        /// </summary>
+        /// <returns>Total number of hours required to work to stock all the coli. Specific to two decimals</returns>
+        public double WorkHoursStockClerk()
         {
-            throw new System.NotImplementedException();
-        }
+            var stockForecast = _forecastStandards.Find(f => f.Activity == ForecastActivity.STOCK_SHELVES);
+            if(stockForecast == null)
+                throw new NullReferenceException("Forecast activity for STOCK_SHELVES was null");
+            var minutesPerColi = stockForecast.Value;
 
-        public int AmountOfPeopleExpected()
-        {
-            throw new System.NotImplementedException();
+            return Math.Round((double)_expectedColi * minutesPerColi / 60, 2);
         }
     }
 }
