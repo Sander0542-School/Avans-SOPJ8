edf46e95e4b9db3cb3f72800c1cc7af8e8fe53c5
Tommy den reijer <t.ommy@hotmail.nl>
2020-11-18 18:45:18 +0100

    function to link employee to department and branch added

--- Bumbo.Web/Controllers/UsersController.cs
+++ Bumbo.Web/Controllers/UsersController.cs
@@ -1,14 +1,12 @@
-﻿using System;
+﻿using Bumbo.Data;
+using Bumbo.Data.Models;
+using Bumbo.Data.Models.Enums;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Bumbo.Data;
-using Bumbo.Data.Models;
-using Microsoft.AspNetCore.Identity;
-using Bumbo.Data.Models.Enums;
 using static Bumbo.Web.Models.Users.DetailsViewModel;
 
 namespace Bumbo.Web.Controllers
@@ -60,20 +58,29 @@ namespace Bumbo.Web.Controllers
         // POST: Users/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(User user)
+        public async Task<IActionResult> Create(CreateUserViewModel model)
         {
             if (ModelState.IsValid)
             {
 
-                var UpdateUser = user;
+                var user = new User
+                {
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    MiddleName = model.MiddleName,
+                    LastName = model.LastName,
+                    PhoneNumber = model.PhoneNumber,
+                    Birthday = model.Birthday,
+                    ZipCode = model.ZipCode,
+                    HouseNumber = model.HouseNumber,
+                    UserName = model.Email
 
-                UpdateUser.UserName = user.Email;
-
-                var result = await _userManager.CreateAsync(UpdateUser);
+                };
+                var result = await _userManager.CreateAsync(user);
                 //await _wrapper.User.Add(user);
                 return RedirectToAction(nameof(Index));
             }
-            return View(user);
+            return View(model);
         }
 
         // GET: Users/Edit/5
@@ -85,25 +92,46 @@ namespace Bumbo.Web.Controllers
             }
 
             var user = await _wrapper.User.Get(user => user.Id == id);
+            List<SelectListItem> branchesList = await GetBranchList();
+
+            var UserDetail = new EditUserViewModel
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                MiddleName = user.MiddleName,
+                LastName = user.LastName,
+                ZipCode = user.ZipCode,
+                HouseNumber = user.HouseNumber,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+
+                UserBranches = user.Branches,
+                Contracts = user.Contracts,
+                Branches = branchesList
+
+
+            };
             if (user == null)
             {
                 return NotFound();
             }
-            return View(user);
+            return View(UserDetail);
         }
+
+       
 
         // POST: Users/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, UserDetail model)
+        public async Task<IActionResult> Edit(int id, EditUserViewModel model)
         {
 
-            //if (id != model.Id)
-            //{
-            //    return NotFound();
-            //}
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
 
             // Get the existing student from the db
             var user =  await _wrapper.User.Get(user => user.Id == id);
@@ -121,8 +149,13 @@ namespace Bumbo.Web.Controllers
             await _userManager.UpdateAsync(user);
 
 
+            //TODO fix dit
+            model.UserBranches = user.Branches;
+            model.Contracts = user.Contracts;
 
-            return View(user);
+
+
+            return View(model);
         }
 
         // GET: Users/Delete/5
@@ -172,22 +205,114 @@ namespace Bumbo.Web.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditDepartment(int id, List<Department> Department)
+        public async Task<IActionResult> EditDepartment(int id, Department department, int branch)
         {
-            //if (id != model.Id)
-            //{
-            //    return NotFound();
-            //}
+            
 
 
             // Get the existing student from the db
             var user = await _wrapper.User.Get(user => user.Id == id);
 
 
+            UserBranch userBranch2 = user.Branches.Where(b => b.BranchId == branch).Where(b => b.Department == department).FirstOrDefault();
+
+            if (userBranch2 != null)
+            {
+                if (userBranch2.Department == department)
+                {
+                    return NotFound();
+                }
+            }
 
 
+            var userBranch = new UserBranch
+            {
+                BranchId = branch,
+                UserId = user.Id,
+                Department = department,
 
-            return View(user);
+
+            };
+
+            user.Branches.Add(userBranch);
+
+            await _wrapper.User.Update(user);
+
+            List<SelectListItem> branchesList = await GetBranchList();
+
+            EditUserViewModel userModel = CreateUserModel(user, branchesList);
+
+            return View("Edit", userModel);
+
+            
         }
+       
+
+        public async Task<IActionResult> DeleteBranchUser(int? id, int? branchId)
+        {
+            if (id == null || branchId == null)
+            {
+                return NotFound();
+            }
+
+
+
+
+            User user = await _wrapper.User.Get(m => m.Id == id);
+            UserBranch userBranch = user.Branches.Where(b => b.BranchId == branchId).FirstOrDefault();
+
+
+
+            user.Branches.Remove(userBranch);
+            await _wrapper.User.Update(user);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            List<SelectListItem> branchesList = await GetBranchList();
+            EditUserViewModel userModel = CreateUserModel(user, branchesList);
+
+            return View("Edit", userModel);
+        }
+
+        private async Task<List<SelectListItem>> GetBranchList()
+        {
+            var branches = await _wrapper.Branch.GetAll();
+
+
+            var branchesList = branches.Select(a =>
+                                  new SelectListItem
+                                  {
+                                      Value = a.Id.ToString(),
+                                      Text = a.Name
+                                  }).ToList();
+            return branchesList;
+        }
+
+        static EditUserViewModel CreateUserModel(User user, List<SelectListItem> branchesList)
+        {
+            return new EditUserViewModel
+            {
+                PhoneNumber = user.PhoneNumber,
+                Id = user.Id,
+                FirstName = user.FirstName,
+                MiddleName = user.MiddleName,
+                LastName = user.LastName,
+                Birthday = user.Birthday,
+                ZipCode = user.ZipCode,
+                HouseNumber = user.HouseNumber,
+                Email = user.Email,
+
+                UserBranches = user.Branches,
+                Contracts = user.Contracts,
+                Branches = branchesList
+
+
+
+            };
+        }
+
     }
 }
--- Bumbo.Web/Models/Users/DetailsViewModel.cs
+++ Bumbo.Web/Models/Users/DetailsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Bumbo.Data.Models.Validators;
+﻿using Bumbo.Data.Models;
+using Bumbo.Data.Models.Validators;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -10,11 +12,18 @@ namespace Bumbo.Web.Models.Users
     public class DetailsViewModel
     {
 
-        public class UserDetail
+        public class EditUserViewModel
         {
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+
+            
+           
+
+            [Required]
+            [Display(Name = "Id")]
+            public int Id { get; set; }
 
             [Required]
             [DataType(DataType.Text)]
@@ -51,6 +60,67 @@ namespace Bumbo.Web.Models.Users
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
+
+            public IList<UserContract> Contracts { get; set; }
+
+            public IList<UserBranch> UserBranches { get; set; }
+
+            public IList<SelectListItem> Branches { get; set; }
+
         }
+
+        public class CreateUserViewModel
+        {
+
+            
+
+            [Phone]
+            [Display(Name = "Phone number")]
+            public string PhoneNumber { get; set; }
+
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "First name")]
+            public string FirstName { get; set; }
+
+            [DataType(DataType.Text)]
+            [Display(Name = "Middle name")]
+            public string MiddleName { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Last name")]
+            public string LastName { get; set; }
+
+            [Required]
+            [DataType(DataType.Date)]
+            [Display(Name = "Birthday")]
+            public DateTime Birthday { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [ZipCode]
+            [Display(Name = "Zip Code")]
+            public string ZipCode { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [BuildingNumber]
+            [Display(Name = "House number")]
+            public string HouseNumber { get; set; }
+
+            public IList<UserContract> Contracts { get; set; }
+
+            public IList<UserBranch> Branches { get; set; }
+
+        }
+
+
     }
 }
