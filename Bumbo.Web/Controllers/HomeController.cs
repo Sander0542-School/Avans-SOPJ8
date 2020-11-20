d6442a372a10e2ca3d7ff1f5eef733f33b8fb572
Celine Loos <liefs_celine@hotmail.com>
2020-11-20 15:24:52 +0100

    :sparkles: Localizer in controller, does nothing yet

--- Bumbo.Web/Controllers/HomeController.cs
+++ Bumbo.Web/Controllers/HomeController.cs
@@ -8,16 +8,19 @@ using Microsoft.Extensions.Logging;
 using Bumbo.Web.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Localization;
 
 namespace Bumbo.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer _localizer;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IStringLocalizer localizer)
         {
             _logger = logger;
+            _localizer = localizer;
         }
 
         public IActionResult Index()
