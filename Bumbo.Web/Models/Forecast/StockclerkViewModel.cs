784963284f0c019ffbe664d40bd9dfd64f5c4e3c
Job <LordChunk@users.noreply.github.com>
2020-11-22 16:52:42 +0100

    Added comments explaining current model implementation

--- Bumbo.Web/Models/Forecast/StockclerkViewModel.cs
+++ Bumbo.Web/Models/Forecast/StockclerkViewModel.cs
@@ -9,6 +9,9 @@ namespace Bumbo.Web.Models.Forecast
         public int BranchId;
         public int DaysInForecast;
 
+        // This is not ideal, but binding data to a list of objects in .NET Core seems to be pretty finicky.
+        // As this works in our current implementation it should be kept like this for now
+        // See this link for previous BROKEN implementation: https://github.com/Sander0542/Bumbo/pull/42/commits/a2f330a2af75f3eb88934aa7dac4f8a494ba2a31
         public List<decimal> MetersOfShelves { get; set; }
         public List<decimal> ExpectedNumberOfColi { get; set; }
     }
