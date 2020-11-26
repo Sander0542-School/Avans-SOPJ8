162a88d03b09467542751fd22fcd9de5194311fa
Sander Jochems <sanderjochems@hotmail.nl>
2020-11-26 16:58:44 +0100

    Update Bumbo.Data/Repositories/UserAdditionalWorkRepository.cs

--- Bumbo.Data/Repositories/UserAdditionalWorkRepository.cs
+++ Bumbo.Data/Repositories/UserAdditionalWorkRepository.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Bumbo.Data.Models;
-using Bumbo.Data.Repositories.Common;
-using Microsoft.EntityFrameworkCore;
 
 namespace Bumbo.Data.Repositories
 {
