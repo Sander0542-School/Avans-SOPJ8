525dcf896a78ebfaba240e3818c1489170b15bdb
Job <LordChunk@users.noreply.github.com>
2020-11-26 12:37:48 +0100

    Forecast uses date instead of datetime

---
+++ Bumbo.Data/Migrations/20201126113554_ForecastDateTimeToDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bumbo.Data.Migrations
+{
+    public partial class ForecastDateTimeToDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Forecast",
+                type: "Date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Forecast",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "Date");
+        }
+    }
+}
