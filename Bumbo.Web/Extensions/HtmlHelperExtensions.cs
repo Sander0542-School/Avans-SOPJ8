fb050d91d79c2f4fb135572ab0b4e32f064a4742
Sander Jochems <sanderjochems@hotmail.nl>
2020-11-26 17:38:22 +0100

    Update HtmlHelperExtensions.cs

--- Bumbo.Web/Extensions/HtmlHelperExtensions.cs
+++ Bumbo.Web/Extensions/HtmlHelperExtensions.cs
@@ -9,24 +9,24 @@ namespace Bumbo.Web.Extensions
     {
         public static string IsActive(this IHtmlHelper htmlHelper, string controllers, string actions = null, string cssClass = "active")
         {
-            string currentController = htmlHelper.ViewContext.RouteData.Values["controller"] as string;
-            string currentAction = htmlHelper.ViewContext.RouteData.Values["action"] as string;
+            var currentController = htmlHelper.ViewContext.RouteData.Values["controller"] as string;
+            var currentAction = htmlHelper.ViewContext.RouteData.Values["action"] as string;
 
             IEnumerable<string> acceptedControllers = (controllers ?? currentController ?? "").Split(',');
             IEnumerable<string> acceptedActions = (actions ?? currentAction ?? "").Split(',');
 
-            return acceptedControllers.Contains(currentController) && (acceptedActions.Contains(currentAction) || currentAction == null) ? cssClass : String.Empty;
+            return acceptedControllers.Contains(currentController) && (acceptedActions.Contains(currentAction) || currentAction == null) ? cssClass : string.Empty;
         }
 
         public static string IsActiveArea(this IHtmlHelper htmlHelper, string area, string pages = null, string cssClass = "active")
         {
-            string currentArea = htmlHelper.ViewContext.RouteData.Values["area"] as string;
-            string currentPage = htmlHelper.ViewContext.RouteData.Values["page"] as string;
+            var currentArea = htmlHelper.ViewContext.RouteData.Values["area"] as string;
+            var currentPage = htmlHelper.ViewContext.RouteData.Values["page"] as string;
 
             IEnumerable<string> acceptedAreas = (area ?? currentArea ?? "").Split(',');
             IEnumerable<string> acceptedPages = (pages ?? currentPage ?? "").Split(',');
 
-            return acceptedAreas.Contains(currentArea) && (acceptedPages.Contains(currentPage) || currentPage == null) ? cssClass : String.Empty;
+            return acceptedAreas.Contains(currentArea) && (acceptedPages.Contains(currentPage) || currentPage == null) ? cssClass : string.Empty;
         }
     }
 }
