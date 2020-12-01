4bfe36365262134d1e7d0e80c690990f10833d92
Job <LordChunk@users.noreply.github.com>
2020-12-01 20:54:34 +0100

    Replaced displayname with display

--- Bumbo.Data/Models/Branch.cs
+++ Bumbo.Data/Models/Branch.cs
@@ -8,19 +8,19 @@ namespace Bumbo.Data.Models
     public class Branch : BaseEntity
     {
         [Required]
-        [DisplayName("Name")]
+        [Display(Name = "Name")]
         public string Name { get; set; }
         
         [StringLength(7, MinimumLength = 6)]
         [RegularExpression(@"^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-zA-Z]{2}$")]
         [Required(ErrorMessage = "Zip Code is Required.")]
-        [DisplayName("Zip code")]
+        [Display(Name = "Zip code")]
         public string ZipCode { get; set; }
         
         [StringLength(7, MinimumLength = 1)]
         [RegularExpression(@"^[1-9][0-9]{0,4}[a-z]{0,2}$")]
         [Required]
-        [DisplayName("House number")]
+        [Display(Name = "House number")]
         public string HouseNumber { get; set; }
         
         
