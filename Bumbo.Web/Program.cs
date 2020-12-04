dd9b068a0922e28bc40e679dcb3ad403830d07d4
Sander Jochems <sanderjochems@hotmail.nl>
2020-12-04 12:22:46 +0100

    Update Program.cs

--- Bumbo.Web/Program.cs
+++ Bumbo.Web/Program.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Bumbo.Data.Seeder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Bumbo.Web
 {
