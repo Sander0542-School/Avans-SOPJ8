4b18a68b05acf5cbeb49a025c361a26c7444d5b2
Mark Vermulst <m.vermulst@student.avans.nl>
2020-12-06 16:38:06 +0100

    Cleaned up some code smells

--- Bumbo.Logic/PayCheck/PayCheck.cs
+++ Bumbo.Logic/PayCheck/PayCheck.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Bumbo.Logic.PayCheck
 {
     public class PayCheck
     {
         private Dictionary<double, TimeSpan> _workedHoursPerBonus;
-        private List<double> _keys = new List<double>
-        {
-            1.0,
-            1.33,
-            1.5,
-            2.0,
-            0.70
-        }; 
+
+        public const double Standard = 1.0;
+        public const double Between20And21Bonus = 1.33;
+        public const double NightBonus = 1.5; 
+        public const double SundayBonus = 2.0;
+        public const double SickBonus = 0.70;
 
         public PayCheck()
         {
             _workedHoursPerBonus = new Dictionary<double, TimeSpan>
             {
-                {_keys[0], new TimeSpan()},
-                {_keys[1], new TimeSpan()},
-                {_keys[2], new TimeSpan()},
-                {_keys[3], new TimeSpan()},
-                {_keys[4], new TimeSpan()}
+                {Standard, new TimeSpan()},
+                {Between20And21Bonus, new TimeSpan()},
+                {NightBonus, new TimeSpan()},
+                {SundayBonus, new TimeSpan()},
+                {SickBonus, new TimeSpan()}
             };
         }
 
@@ -31,9 +30,8 @@ namespace Bumbo.Logic.PayCheck
         {
             if (!_workedHoursPerBonus.ContainsKey(key))
                 throw new ArgumentNullException("KeyNotFound", "The Given Key Does Not exist. Did you try to look for a bonus that is not used?");
-            _workedHoursPerBonus.TryGetValue(key, out var hoursAlreadyInTheBonus);
-            _workedHoursPerBonus.Remove(key);
-            _workedHoursPerBonus.Add(key, hoursAlreadyInTheBonus + timeSpan);
+
+            _workedHoursPerBonus[key].Add(timeSpan);
         }
 
         public TimeSpan GetTime(double key)
@@ -46,20 +44,20 @@ namespace Bumbo.Logic.PayCheck
 
         public void AddPayCheck(PayCheck payCheck)
         {
-            foreach (var kvp in payCheck.getPayCheck())
+            foreach (var kvp in payCheck.GetPayCheck())
             {
                 AddTime(kvp.Key,kvp.Value);
             }
         }
 
-        public Dictionary<double, TimeSpan> getPayCheck()
+        public Dictionary<double, TimeSpan> GetPayCheck()
         {
             return _workedHoursPerBonus;
         }
 
-        public List<double> getKeys()
+        public List<double> GetKeys()
         {
-            return _keys;
+            return _workedHoursPerBonus.Keys.ToList();
         }
     }
 }
