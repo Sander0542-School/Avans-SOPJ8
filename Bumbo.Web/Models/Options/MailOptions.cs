87d7b8363d184d1e567b6fb4eff33c3d96add135
Sander Jochems <sanderjochems@hotmail.nl>
2021-01-09 20:05:30 +0100

    Fix EmailSender
    Co-Authored-By: Tommy den Reijer <30556476+tommydr@users.noreply.github.com>

--- Bumbo.Web/Models/Options/MailOptions.cs
+++ Bumbo.Web/Models/Options/MailOptions.cs
@@ -5,7 +5,7 @@ namespace Bumbo.Web.Models.Options
         public const string Mail = "Mail";
         
         public string Host { get; set; }
-        public int Port { get; set; } = 587;
+        public int Port { get; set; } = 465;
         public string UserName { get; set; }
         public string Password { get; set; }
         public bool UseTls { get; set; } = true;
