793b9157d8724413ce715773a5226551eef95b9f
Celine <celine@tambien.nl>
2021-01-11 14:23:26 +0100

    🐛 Fixed branched/branchid in url

--- Bumbo.Web/Models/Furlough/ManagerFurloughViewModel.cs
+++ Bumbo.Web/Models/Furlough/ManagerFurloughViewModel.cs
@@ -18,6 +18,7 @@ namespace Bumbo.Web.Models.Furlough
         public class Furlough
         {
             public int Id { get; set; }
+            public int BranchId { get; set; }
 
             public int UserId { get; set; }
 
