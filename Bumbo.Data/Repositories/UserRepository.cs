83e95b9ce6577c4c33f12a14adddd892b1424340
Sander Jochems <sanderjochems@hotmail.nl>
2021-01-12 14:23:36 +0100

    Update UserRepository.cs

--- Bumbo.Data/Repositories/UserRepository.cs
+++ Bumbo.Data/Repositories/UserRepository.cs
@@ -58,22 +58,31 @@ namespace Bumbo.Data.Repositories
         public async Task<List<User>> GetFreeEmployees(int branchId, DateTime date, Department department)
         {
             return await Context.Users
-                .Where(user => user.Branches.Any(b => b.BranchId == branchId && b.Department == department)) // Check if user works at branch and department
-                .Where(user => user.Contracts.Any(contract => contract.StartDate < date)) // Check for active contract
+                .Where(user => user.Branches.Any(b => b.BranchId == branchId && b.Department == department))// Check if user works at branch and department
+                .Where(user => user.Contracts.Any(contract => contract.StartDate < date))// Check for active contract
                 .Where(user => user.Contracts.Any(contract => contract.EndDate >= date))
-                .Where(user => user.Shifts.All(s => s.Date != date)) // Get users who do not have any shifts on that day
+                .Where(user => user.Shifts.All(s => s.Date != date))// Get users who do not have any shifts on that day
                 .ToListAsync();
         }
 
         public async Task<List<User>> GetUpcomingBirthdays(IEnumerable<int> branches, int limit)
         {
-            return await Context.Users
-                .Where(user => user.Birthday != null)
-                .Where(user => user.Branches.Any(branch => branches.Contains(branch.BranchId)))
-                .OrderBy(user => EF.Functions.DateDiffDay(DateTime.Today, user.Birthday.AddYears(EF.Functions.DateDiffYear(user.Birthday, DateTime.Today) + ((user.Birthday.Month < DateTime.Today.Month || (user.Birthday.Day <= DateTime.Today.Day && user.Birthday.Month == DateTime.Today.Month)) ? 1 : 0))))
-                .Take(limit)
-                .AsSplitQuery()
-                .ToListAsync();
+            try
+            {
+                return await Context.Users
+                    .Where(user => user.Birthday != null)
+                    .Where(user => user.Branches.Any(branch => branches.Contains(branch.BranchId)))
+                    .OrderBy(user => EF.Functions.DateDiffDay(DateTime.Today, user.Birthday.AddYears(EF.Functions.DateDiffYear(user.Birthday, DateTime.Today) + ((user.Birthday.Month < DateTime.Today.Month || (user.Birthday.Day <= DateTime.Today.Day && user.Birthday.Month == DateTime.Today.Month)) ? 1 : 0))))
+                    .Take(limit)
+                    .AsSplitQuery()
+                    .ToListAsync();
+            }
+            catch (InvalidOperationException)
+            {
+                if (!Context.Database.IsSqlite()) throw;
+            }
+            
+            return new List<User>();
         }
 
         public async Task<List<User>> GetSickEmployees(IEnumerable<int> branches, DateTime date)
