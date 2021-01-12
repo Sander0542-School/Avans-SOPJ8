367fa93f5139ee02fe97329fd976e68578fa8c59
Sander Jochems <sanderjochems@hotmail.nl>
2021-01-12 14:38:16 +0100

    Update ConfigureServices.cs

--- Bumbo.Web/ConfigureServices.cs
+++ Bumbo.Web/ConfigureServices.cs
@@ -9,8 +9,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using OpenWeatherMap.Cache;
-using OpenWeatherMap.Cache.Extensions;
 
 namespace Bumbo.Web
 {
