c8f89165197f5aaa8dd5eae02065719ab035e013
Tommy den reijer <t.ommy@hotmail.nl>
2021-01-13 19:02:55 +0100

    added users create tests

---
+++ Bumbo.Tests.Cypress/integration/users/2-add-users.ts
@@ -0,0 +1,29 @@
+describe('show users', () => {
+    beforeEach(() => {
+      // Keeps session alive through this "describe" method
+      Cypress.Cookies.preserveOnce('.AspNetCore.Identity.Application');
+    });
+  
+    it('Login for manager user', () => {
+      cy.login('super');
+    });
+  
+    it('Navigate users index page', () => {
+      cy.visit('Users/Create');
+
+      cy.get('#FirstName').type('Test');
+      cy.get('#MiddleName').type('16:00');
+      cy.get('#LastName').type('User');
+      cy.get('#Birthday').type('2000-03-20');
+      cy.get('#ZipCode').type('5500 el');
+      cy.get('#HouseNumber').type('1');
+      cy.get('#Email').type('Test@user.nl');
+      cy.get('#PhoneNumber').type('31677889900');
+
+      cy.get('input[type="submit"]').should('be.visible').click();
+
+      cy.get('table tr').should('be.visible');
+    });
+  
+    
+  });
