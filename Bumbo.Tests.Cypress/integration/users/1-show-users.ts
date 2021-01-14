7393831b1cdbf2f57111bf0d4b85fcf97f754713
Tommy den reijer <t.ommy@hotmail.nl>
2021-01-14 11:01:07 +0100

    Format user test

--- Bumbo.Tests.Cypress/integration/users/1-show-users.ts
+++ Bumbo.Tests.Cypress/integration/users/1-show-users.ts
@@ -1,18 +1,18 @@
 describe('show users', () => {
-    beforeEach(() => {
-      // Keeps session alive through this "describe" method
-      Cypress.Cookies.preserveOnce('.AspNetCore.Identity.Application');
-    });
-  
-    it('Login for manager user', () => {
-      cy.login('super');
-    });
-  
-    it('Navigate users index page', () => {
-      cy.visit('Users');
-      cy.get('#userTable').should('exist');
-      
-    });
-  
-    
+  beforeEach(() => {
+    // Keeps session alive through this "describe" method
+    Cypress.Cookies.preserveOnce('.AspNetCore.Identity.Application');
   });
+
+  it('Login for manager user', () => {
+    cy.login('super');
+  });
+
+  it('Navigate users index page', () => {
+    cy.visit('Users');
+    cy.get('#userTable').should('exist');
+
+  });
+
+
+});
